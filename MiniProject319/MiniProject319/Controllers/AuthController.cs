948d7a978192b16c777b0d011e0ec075ddc16028
Fachri Kusnaini <pookiyu4@gmail.com>
2023-07-14 23:26:05 +0700

    Add template Login, and added link forget password and create account

---
+++ MiniProject319/MiniProject319/Controllers/AuthController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MiniProject319.Models;
+using System.Diagnostics;
+
+namespace MiniProject319.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly ILogger<AuthController> _logger;
+
+        public AuthController(ILogger<AuthController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult _Layout()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
