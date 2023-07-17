488c86f72af48bcdfc70ddcc521eb0e8058c232d
ujangdotnet <ujangdotnet@gmail.com>
2023-07-17 16:33:18 +0700

    Day 2: Edit Layout Landing Page and Layout Profile, membuat ER Diagram

---
+++ MiniProject319/MiniProject319/Controllers/Profile.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MiniProject319.Controllers
+{
+    public class Profile : Controller
+    {
+        public IActionResult Profile_Dokter()
+        {
+            return View();
+        }
+    }
+}
