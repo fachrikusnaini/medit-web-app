0f7bca9eb66e39b2b33d7a2a6ca419c68fb2f79b
AbiyuDK <abiyudk09@gmail.com>
2023-07-17 16:48:03 +0700

    second commit

---
+++ MiniProject319/MiniProject319/Controllers/PofileController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MiniProject319.Controllers
+{
+    public class PofileController : Controller
+    {
+        public IActionResult Index()
+        {
+            return PartialView();
+        }
+    }
+}
